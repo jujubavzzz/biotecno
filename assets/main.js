68d5961e6e5a6a39672db25fd3bf685bf34aed37
jujubavzzz <166621143+jujubavzzz@users.noreply.github.com>
2024-11-26 08:37:38 -0300

    l

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+function criacartao (pergunta, resposta) {
+    let conteudo = document.getElementById (' conteudo')
+    let cartao = document. createElement ('article')
+    cartao. className = cartao'
+
+    cartao. innerHTML =
+     <div class= "cartao- -conteudo">
+    <div class="cartao--pergunta"><p>${pergunta)</p> </div>
+    <div class="cartao--resposta"><p>$(resposta)</p› </div>
+    </div>
+
+    console. log (cartao)
+
+    let respostaEstaVisivel = false
+
+    function viraCartao () {
+      respostaEstaVisivel = 1respostaEstaVisivel
+      cartao.classList. toggle('active', respostaEstaVisivel)}
+
+    cartao.addEventListener('click', viracartao) 
+    conteudo. appendChild (cartao)
