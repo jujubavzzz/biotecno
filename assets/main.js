2e6a704a13bd688907655fbaad9fc0b64566a331
jujubavzzz <166621143+jujubavzzz@users.noreply.github.com>
2024-11-28 22:15:20 -0300

    s

--- assets/main.js
+++ assets/main.js
@@ -1,21 +1,21 @@
 function criacartao (pergunta, resposta) {
     let conteudo = document.getElementById (' conteudo')
     let cartao = document. createElement ('article')
-    cartao. className = cartao
+    cartao. className = ' cartao '
 
     cartao. innerHTML =
      <div class= "cartao- -conteudo">
     <div class="cartao--pergunta"><p>$(pergunta)</p> </div>
-    <div class="cartao--resposta"><p>$(resposta)</p› </div>
+    <div class="cartao--resposta"><p>$(resposta)</p> </div>
     </div>
-
     console. log (cartao)
 
     let respostaEstaVisivel = false
 
     function viraCartao () {
-      respostaEstaVisivel = 1respostaEstaVisivel
+      respostaEstaVisivel =! respostaEstaVisivel
       cartao.classList. toggle('active', respostaEstaVisivel)}
 
     cartao.addEventListener('click', viracartao) 
     conteudo. appendChild (cartao)
+  }
