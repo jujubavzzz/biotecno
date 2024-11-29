0fcadb554cde87d78d8ff85a5086a9e6c43b4309
jujubavzzz <166621143+jujubavzzz@users.noreply.github.com>
2024-11-28 22:17:14 -0300

    j

--- pergunta.js
+++ pergunta.js
@@ -1,26 +1,26 @@
 criaCartao(
-    " O QUE É BIOTECNOLOGIA VERDE E COMO ELA SE DIFERENCIA DE OUTRAS FORMAS DE BIOTECNOLOGIA?"*
+    " O QUE É BIOTECNOLOGIA VERDE E COMO ELA SE DIFERENCIA DE OUTRAS FORMAS DE BIOTECNOLOGIA?"
     "Responsável pela agricultura. Nutrir e fertilizar as plantações e combater pragas e fenômenos climatológicos.")
 
 criaCartao(
-" COMO A BIOTECNOLOGIA VERDE PODE AJUDAR NA CONSERVAÇÃO DA BIODIVERSIDADE?"*
-"Via desenvolvimento de tecnologias de preservação, de restauração de terras degradadas, de redução da poluição e de produtos para reduzir perdas de pós-colheita.")
+    " COMO A BIOTECNOLOGIA VERDE PODE AJUDAR NA CONSERVAÇÃO DA BIODIVERSIDADE?"
+    "Via desenvolvimento de tecnologias de preservação, de restauração de terras degradadas, de redução da poluição e de produtos para reduzir perdas de pós-colheita.")
 
 criaCartao(
-    "COMO A BIOTECNOLOGIA VERDE PODE CONTRIBUIR PARA  A REDUÇÃO DO USO DE PESTICIDAS?"*
+    "COMO A BIOTECNOLOGIA VERDE PODE CONTRIBUIR PARA  A REDUÇÃO DO USO DE PESTICIDAS?"
     "Colonizando as suas raízes com organismos benéficos que estimulam o sistema imunológico dos vegetais.")
 
 criaCartao(
-    "DE QUE MANEIRA A BIOTECNOLOGIA VERDE PODE AJUDAR A MISTIGAR OS EFEITOS DAS MUDANÇAS CLIMÁTICAS?"*
+    "DE QUE MANEIRA A BIOTECNOLOGIA VERDE PODE AJUDAR A MISTIGAR OS EFEITOS DAS MUDANÇAS CLIMÁTICAS?"
     "A biotecnologia acelera o melhoramento genético de plantas, animais e microrganismos.")
 
 criaCartao(
-        "QUAIS SÃO AS IMPLICAÇÕES DA BIOTECNOLOGIA VERDE NA SEGURANÇA ALIMENTAR GLOBAL?"*
+        "QUAIS SÃO AS IMPLICAÇÕES DA BIOTECNOLOGIA VERDE NA SEGURANÇA ALIMENTAR GLOBAL?"
         "Facilita a produção de proteínas alternativas, como proteínas vegetais e carne cultivada em laboratório.")
 
 criaCartao(
-" COMO OS  AVANÇOS EM TÉCNICA DE EDIÇÃO GENÉTICA ESTÃO SENDO UTILIZADO ATUALMENTE?"*
-" O sistema CRISPR tem enorme potencial de acelerar o melhoramento genético de plantas, auxiliando os agricultores a superarem os principais desafios que acometem as lavouras.")
+       " COMO OS  AVANÇOS EM TÉCNICA DE EDIÇÃO GENÉTICA ESTÃO SENDO UTILIZADO ATUALMENTE?"
+        " O sistema CRISPR tem enorme potencial de acelerar o melhoramento genético de plantas, auxiliando os agricultores a superarem os principais desafios que acometem as lavouras.")
 
     
 
