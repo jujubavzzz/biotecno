44d4eb49b62e2f34103ce4bd35b9983eadf2ba20
jujubavzzz <166621143+jujubavzzz@users.noreply.github.com>
2024-12-02 18:39:35 -0300

    l

--- pergunta.js
+++ pergunta.js
@@ -1,10 +1,10 @@
 
 criaCartao(
-    " O QUE É BIOTECNOLOGIA VERDE E COMO ELA SE DIFERENCIA DE OUTRAS FORMAS DE BIOTECNOLOGIA?",
+    "O QUE É BIOTECNOLOGIA VERDE E COMO ELA SE DIFERENCIA DE OUTRAS FORMAS DE BIOTECNOLOGIA?",
     "Responsável pela agricultura. Nutrir e fertilizar as plantações e combater pragas e fenômenos climatológicos.")
 
 criaCartao(
-    " COMO A BIOTECNOLOGIA VERDE PODE AJUDAR NA CONSERVAÇÃO DA BIODIVERSIDADE?",
+    "COMO A BIOTECNOLOGIA VERDE PODE AJUDAR NA CONSERVAÇÃO DA BIODIVERSIDADE?",
     "Via desenvolvimento de tecnologias de preservação, de restauração de terras degradadas, de redução da poluição e de produtos para reduzir perdas de pós-colheita.")
 
 criaCartao(
